8adadf7a917e5ed3c2b2cfd1afccbb0514234a88
Tahn Rainbird <14843@holbertonstudents.com>
2026-07-07 00:41:06 +0000

    new script

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - use the _putchar function defined in main.h to print _putchar
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	_putchar();
+
+	return (0);
+}
