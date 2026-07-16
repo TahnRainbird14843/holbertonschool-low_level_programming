2b1260855e5c3aea623a53ad3bef606d852fdca7
Tahn Rainbird <14843@holbertonstudents.com>
2026-07-16 00:19:44 +0000

    fixes

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
  * _strchr - returns a pointer to the first occurence of character
@@ -19,5 +18,5 @@ char *_strchr(char *s, char c)
 		i++;
 	}
 
-	return (NULL);
+	return ("");
 }
